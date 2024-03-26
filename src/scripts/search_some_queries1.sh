97c63c21dd2e7ec59480987925e6fdedd9c99eab
Thomas Schnake <t.schnake@tu-berlin.de>
2024-03-26 20:54:45 +0100

    ...

--- src/scripts/search_some_queries1.sh
+++ src/scripts/search_some_queries1.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
-python run_query_auto_search.py --sample_range [259] --comp_mode 'harsanyi' --max_and_order 3 --harsanyi_maxorder 1 --weight_modes "['occlusion','shapley']" 
+# Initialize boolean variable
+on_cluster=false
+
+# Check if "--on_cluster" argument is present
+if [[ "$1" == "--on_cluster" ]]; then
+    on_cluster=true
+fi
+
+# Use the boolean variable as needed
+if [ "$on_cluster" = true ]; then
+	script_path="/home/thomas_schnake/ResearchProjects/symbXAI_project/symbolicXAI_code/src/scripts/"
+else
+	script_path="/Users/thomasschnake/Research/Projects/symbolic_xai/symbolicXAI_github/src/scripts/"
+fi
+
+
+
+python ${script_path}run_query_auto_search.py --sample_range [259] --comp_mode 'harsanyi' --max_and_order 3 --harsanyi_maxorder 1 --weight_modes "['occlusion','shapley']" 
 
 
