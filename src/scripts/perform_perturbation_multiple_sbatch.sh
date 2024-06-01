ea7e422f881ed55067494e403a3a5a97f3c12036
thomas_schnake <t.schnake@tu-berlin.de>
2024-06-02 01:11:18 +0200

    ,,

--- src/scripts/perform_perturbation_multiple_sbatch.sh
+++ src/scripts/perform_perturbation_multiple_sbatch.sh
@@ -6,15 +6,21 @@ resultfolder="/home/thomas_schnake/ResearchProjects/symbXAI_project/local_experi
 # Define the variable dry_run
 dry_run="false"
 
-data_mode="sst"
+data_mode="imdb"
 # Check if dry_run is true
 if [ "$dry_run" = "true" ]; then
   range='0 1'
 else
-  range=$(seq 0 200)
+  range=$(seq 0 100)
 fi
 
-for ids in $range; do
-  sbatch --mem=15G perform_perturbation_apptainer_wrapper.sh --sample_range "[${ids}]" --result_dir "${resultfolder}" --data_mode "${data_mode}"
-
+# Define the two lists
+list1=("minimize" "maximize")
+list2=("removal" "generation")
+for auc_task in "${list1[@]}"; do
+    for perturbation_type in "${list2[@]}"; do
+        for ids in $range; do
+          sbatch --mem=15G perform_perturbation_apptainer_wrapper.sh --sample_range "[${ids}]" --result_dir "${resultfolder}" --data_mode "${data_mode}" --auc_task "$auc_task" --perturbation_type "$perturbation_type"
+        done
+    done
 done
